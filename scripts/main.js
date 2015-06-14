308075e9d5c2e3608fab28a23d06aeadf0199ec4
chris bustamante <chrisbusta@hotmsil.com>
2015-06-14 13:56:46 -0400

    inital changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,3 @@
-// var sidebar = items.results.map( function(item){
-//   return item.taxonomy_path;
-// });
-
-
-// var sidebarItems = [];
-//  sidebar.forEach( function (list) {
-//   list.forEach( function (item) {
-//     if (sidebarItems.indexof(item) === -1){
-//       sidebarItems.push(item);
-//     }
-//   });
-//  });
-
-
-//  var sideBarUL = $('#sideBarCats');
-
-//  sidebarItems.forEach( function(sbItem) {
-//   sideBarUL.append("<li><a href='#'>" + sbItem  + "</a></li>");
-//  });
 
   $(document).ready(function(){
 
@@ -46,10 +26,6 @@
     resultsof.append(resultsbin);
 
 
-    // itemHolder = "<li class ='itemData'>" + "<a href = 'http://etsy.com/listing/' + itemListLink + "'>" +itemImage + "</a>" + "<div class = 'itemText'>" + "<a class='title' href ='https://www.etsy.com/listing/" + itemListLink/" + "'>" + itemTitle + "</a>" + "<a href='" + "https://www.etsy.com/shop/" + itemSellerLink + "'>" + itemSeller + "</a>" + itemPrice + "</div>" + "</li>";
-
-
-    // $itemData.append(itemHolder);
 
 
 
